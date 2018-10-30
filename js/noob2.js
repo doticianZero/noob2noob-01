a9c2e6596f42dee87810d84125c1091d3db003d4
Nikos Glezakos <nick@doticca.com>
2018-10-30 18:59:43 +0200

    If function for <a> append

--- js/noob2.js
+++ js/noob2.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 var div01 = $("div").eq(0);
 var div02 = $("div").eq(1);
 var div03 = $("div").eq(2);
+var appendedA = false;
 
 
    div01.append ("<p>Please push the button</p>");
@@ -12,7 +13,10 @@ var div03 = $("div").eq(2);
         .children("a");   
 
 var PushThebutton = function (){
-    div03.append ("<p>You pressed the button!</p>")
+    if (appendedA === false){
+        div03.append ("<p>You pressed the button!</p>")
+    }
+    appendedA === true;
 }
    
 var theChosenOne = $("a").data( "guid", "LukeIamyourfather");      
