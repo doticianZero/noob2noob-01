2fb80a48fc501fa4456a8ab93cf925c5e4d4ca12
Nikos Glezakos <nick@doticca.com>
2018-10-31 20:26:58 +0200

    yes

--- Lesson02/js/noob.js
+++ Lesson02/js/noob.js
@@ -8,14 +8,12 @@ div01.append ("<p>Please push the button </p>");
    
 div02.append ("<a href=# data-guid=123 >Click here and go now</a>")
  
-//var chosenA$ = $("a[data-guid=123]");
-
 $("a[data-guid=123]").click(PushThebutton);
 
 function PushThebutton (){
-        //if (!$("p[data-guid=567]")){
+        if (!$("p[data-guid=567]")){
         div03.append ("<p data-guid=567>You pressed the button!</p>")
-        //};  
+        };  
            }
 });
 
