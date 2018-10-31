485df0aec96a219531002f61aa642690b6aa27fb
doticianZero <44505716+doticianZero@users.noreply.github.com>
2018-10-31 18:51:53 +0200

    Delete noob.js

--- Lesson02/js/noob.js
+++
@@ -1,6 +0,0 @@
-
-var pelem = document.createElement("p");
-var node = document.createTextNode("Hello World!");
-pelem.appendChild(node);
-var element = document.getElementById("par01");
-element.appendChild(pelem);
