a5a5ad421d88f7ba40eb06651cb1946e585e2b56
Nikos Glezakos <nick@doticca.com>
2018-10-31 23:38:22 +0200

    yes

--- Lesson02/js/noob.js
+++ Lesson02/js/noob.js
@@ -1,23 +1,23 @@
 $(document).ready(function(){
 
-var div01 = $("div").eq(0);
-var div02 = $("div").eq(1);
-var div03 = $("div").eq(2);
-var chosen$ = $("p[data-guid='567']");
+        var div01 = $("div").eq(0);
+        var div02 = $("div").eq(1);
+        var div03 = $("div").eq(2);
 
-div01.append ("<p>Please push the button </p>");
-   
-div02.append ("<a href=# data-guid='123' >Click here and go now</a>")
- 
-$("a[data-guid='123']").click(PushThebutton);
-chosen$.data('guid','567')
 
-function PushThebutton (){
-        if (!chosen$.hasAttribute('guid')){
-        div03.append ("<p data-guid='567'>You pressed the button!</p>")
+        div01.append ("<p>Please push the button </p>");
+        
+        div02.append ("<a href='#' data-guid='123' >Click here and go now</a>")
+        
+        $("a[data-guid='123']").click(PushThebutton);
+        //chosen$.data('guid','567')
 
-        };  
-           }
+        function PushThebutton (){
+                var chosen$ = $("p[data-guid='567']");
+                if (!chosen$.hasAttribute('guid')){
+                        div03.append ("<p data-guid='567'>You pressed the button!</p>")
+                };  
+        }
 });
 
 
