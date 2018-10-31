dae88366878d02983e2becf4cd963edf0e7f9a77
Nikos Glezakos <nick@doticca.com>
2018-10-31 22:04:19 +0200

    yes

--- Lesson02/js/noob.js
+++ Lesson02/js/noob.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
 var div01 = $("div").eq(0);
 var div02 = $("div").eq(1);
 var div03 = $("div").eq(2);
-var chosen$ = $("p[data-guid='567']");
 
 div01.append ("<p>Please push the button </p>");
    
@@ -13,9 +12,9 @@ $("a[data-guid='123']").click(PushThebutton);
 
 
 function PushThebutton (){
-        if (chosen$==undefined){
+        if ($("p[data-guid='567']")==undefined){
         div03.append ("<p data-guid='567'>You pressed the button!</p>")
-        chosen$.data (data-guid,'567');
+
         };  
            }
 });
