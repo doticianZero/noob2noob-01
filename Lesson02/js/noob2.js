46d0dcd125234a8d476e7fde0948552d9b8e2dc6
Nikos Glezakos <nick@doticca.com>
2018-10-31 18:49:04 +0200

    New folders

---
+++ Lesson02/js/noob2.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+var div01 = $("div").eq(0);
+var div02 = $("div").eq(1);
+var div03 = $("div").eq(2);
+var oneClickOnly = false;
+
+
+div01.append ("<p>Please push the button</p>");
+   
+div02.append ("<a href=# data-guid=123 >Click here and go now</a>")
+
+ 
+var chosen$ = $("a[data-guid=123]");
+
+chosen$.click(PushThebutton);
+
+
+
+function PushThebutton (){
+        if (oneClickOnly == false){
+        div03.append ("<p>You pressed the button!</p>")    
+        };
+        oneClickOnly=true;
+
+           }
+
+
+
+});
