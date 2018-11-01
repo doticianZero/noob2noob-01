d78c3475546f0e03af5893f405785938a9229579
Nikos Glezakos <nick@doticca.com>
2018-11-01 20:07:33 +0200

    button

--- Practice01/js/noob.js
+++ Practice01/js/noob.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
         var div01 = $("div").eq(0);
         var div02 = $("div").eq(1);
         var div03 = $("div").eq(2);
+        var div04 = $("div").eq(3);
+        var div05 = $("div").eq(5);
 
 
 
@@ -10,7 +12,7 @@ $(document).ready(function(){
         
         div02.append ("<a href='#' data-guid='123' >Click here and go now</a>")
        
-        //div03.append (<button>Show me</button>)
+        div04.append (<button>Show me</button>)
         
         $("a[data-guid='123']").click(PushThebutton);
         //chosen$.data('guid','567')
