cedfd2f0f9ad70502e7dace53b290c54621d5a5a
Nikos Glezakos <nick@doticca.com>
2018-11-01 20:18:40 +0200

    yes

--- Practice01/js/noob.js
+++ Practice01/js/noob.js
@@ -27,7 +27,8 @@ $(document).ready(function(){
         
         $("body").append ("<div data-guid='222'></div>");
         var div04 = $("div[data-guid='222']");
-        div04.load("external.html");
+        div04.append ("<p>hello</p>");
+        //div04.load("external.html");
         
 
 });
