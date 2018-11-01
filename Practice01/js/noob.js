2acb0ec0f8cfb2be0f2bc2bf59c0b6ce5f401f30
Nikos Glezakos <nick@doticca.com>
2018-11-01 20:12:27 +0200

    yes

--- Practice01/js/noob.js
+++ Practice01/js/noob.js
@@ -3,8 +3,7 @@ $(document).ready(function(){
         var div01 = $("div").eq(0);
         var div02 = $("div").eq(1);
         var div03 = $("div").eq(2);
-        var div04 = $("div").eq(3);
-        var div05 = $("div").eq(5);
+        
 
 
 
@@ -12,9 +11,9 @@ $(document).ready(function(){
         
         div02.append ("<a href='#' data-guid='123' >Click here and go now</a>")
        
-        div04.append (<button>Show me</button>)
         
-        /*$("a[data-guid='123']").click(PushThebutton);
+        
+        $("a[data-guid='123']").click(PushThebutton);
         chosen$.data('guid','567')
 
         
@@ -24,11 +23,11 @@ $(document).ready(function(){
                         div03.append ("<p data-guid='567'>You pressed the button!</p>")
                 };  
         }
-        */
         
-        //$("body").append (<div data-guid='222'></div>);
-        //var div04 = $("div[data-guid='222']");
-       // div04.load("external.html");
+        
+        $("body").append ("<div data-guid='222'></div>");
+        var div04 = $("div[data-guid='222']");
+        div04.load("external.html");
 
 
 });
