e70daf6295f71468d06c00b81a58cbcc4b0df4e7
Nikos Glezakos <nick@doticca.com>
2018-11-01 18:51:50 +0200

    prectice 01

---
+++ Practice01/js/noob.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+        var div01 = $("div").eq(0);
+        var div02 = $("div").eq(1);
+        var div03 = $("div").eq(2);
+
+
+        div01.append ("<p>Please push the button </p>");
+        
+        div02.append ("<a href='#' data-guid='123' >Click here and go now</a>")
+        
+        $("a[data-guid='123']").click(PushThebutton);
+        //chosen$.data('guid','567')
+
+        function PushThebutton (){
+                var chosen$ = $("p[data-guid='567']");
+                if (chosen$.length === 0){
+                        div03.append ("<p data-guid='567'>You pressed the button!</p>")
+                };  
+        }
+});
+
+
+
