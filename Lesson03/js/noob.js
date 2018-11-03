18e6a308f071e850d03debc0c0671ee09113bc4f
Nikos Glezakos <nick@doticca.com>
2018-11-03 17:17:19 +0200

    hidden

--- Lesson03/js/noob.js
+++ Lesson03/js/noob.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
         createUI();
 
         function createUI (){
+                createDiv00();
                 createDiv01();
                 createDiv02();
                 createDiv03();
@@ -12,18 +13,26 @@ $(document).ready(function(){
 
         // Create DIVs
 
+        function createDiv00 (){
+                $("body").append ("div data-guid='div00'></div>");
+                $("div[data-guid='div00'")
+                        .append ("<a href=# class='hidden'> tab 1 </a>")
+                        .append ("<a href=# class='hidden'> tab 2 </a>")
+                        .append ("<a href=# class='hidden'> tab 3 </a>")
+        }
+
         function createDiv01 (){
-                $("body").append ("<div data-guid='div01'></div>");
+                $("body").append ("<div data-guid='div01' class='hidden'></div>");
                 $("div[data-guid='div01']").append ("<p>first div</p>")
         }
 
         function createDiv02 (){
-                $("body").append ("<div data-guid=dv02></div>");
+                $("body").append ("<div data-guid=dv02 class='hidden'></div>");
                 $("div[data-guid='dv02']").append ("<p>second div</p>")
         }
 
         function createDiv03 (){
-                $("body").append ("<div data-guid=div03></div>");
+                $("body").append ("<div data-guid=div03 class='hidden'></div>");
                 $("div[data-guid='div03']").append ("<p>third div</p>")
         }
 
