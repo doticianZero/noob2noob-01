4773ef9d8cdd015699250d8e7de11b521fdd4645
Nikos Glezakos <nick@doticca.com>
2018-11-03 20:24:01 +0200

    testng this

--- Lesson03/js/noob.js
+++ Lesson03/js/noob.js
@@ -16,38 +16,46 @@ $(document).ready(function(){
         function createDiv00 (){
                 $("body").append ("<div data-guid='div00'></div>");
                 $("div[data-guid='div00'")
-                        .append ("<a href=# data-target='div01' data-targettype='tab'> tab 1 </a>")
-                        .append ("<a href=# data-target='div02' data-targettype='tab'> tab 2 </a>")
-                        .append ("<a href=# data-target='div03' data-targettype='tab'> tab 3 </a>")
+                        .append ("<a href='#' data-target='div01' data-targettype='tab'> tab 1 </a>")
+                        .append ("<a href='#' data-target='div02' data-targettype='tab'> tab 2 </a>")
+                        .append ("<a href='#' data-target='div03' data-targettype='tab'> tab 3 </a>")
         }
 
         function createDiv01 (){
-                $("body").append ("<div data-guid='div01' class='hidden'></div>");
+                $("body").append ("<div data-guid='div01' data-type='tab' class='hidden'></div>");
                 $("div[data-guid='div01']").append ("<p>first div</p>")
         }
 
         function createDiv02 (){
-                $("body").append ("<div data-guid='div02' class='hidden'></div>");
+                $("body").append ("<div data-guid='div02' data-type='tab' class='hidden'></div>");
                 $("div[data-guid='div02']").append ("<p>second div</p>")
         }
 
         function createDiv03 (){
-                $("body").append ("<div data-guid='div03' class='hidden'></div>");
+                $("body").append ("<div data-guid='div03' data-type='tab' class='hidden'></div>");
                 $("div[data-guid='div03']").append ("<p>third div</p>")
         }
 
 
-        // Controls
+        // Business
+
+        handleTargets();
 
         function handleTargets (){
-                $("a[data-targettype='tab']").click (handleTab());
+                $("a[data-targettype='tab']").click (handleTab(this));
+                
         }
 
-        function handleTab (){
-
+        function handleTab (this){
+                hideAllTabs();
+                var tabcontent$ = this.data('data-target');
         }
 
-       
+        function hideAllTabs (){
+                $("a[data-targettype='tab'").css ("class","hidden");
+        }
+
+
 });
 
 
