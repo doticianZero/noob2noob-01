366d8578747620e97dce198358a2fb0730a8c19d
Nikos Glezakos <nick@doticca.com>
2018-11-03 16:52:00 +0200

    create divs and Ps

--- Lesson03/js/noob.js
+++ Lesson03/js/noob.js
@@ -1,23 +1,31 @@
 $(document).ready(function(){
+       
+        // Create UI
 
-        var div01 = $("div").eq(0);
-        var div02 = $("div").eq(1);
-        var div03 = $("div").eq(2);
-
-
-        div01.append ("<p>Please push the button </p>");
-        
-        div02.append ("<a href='#' data-guid='123' >Click here and go now</a>")
-        
-        $("a[data-guid='123']").click(PushThebutton);
-        //chosen$.data('guid','567')
-
-        function PushThebutton (){
-                var chosen$ = $("p[data-guid='567']");
-                if (chosen$.length === 0){
-                        div03.append ("<p data-guid='567'>You pressed the button!</p>")
-                };  
+        function createUI (){
+                createDiv01();
+                createDiv02();
+                createDiv03();
         }
+
+        // Create DIVs
+
+        function createDiv01 (){
+                $("body").append ("<div data-guid='div01'></div>");
+                $("div[data-guid='div01']").append ("<p>first div</p>")
+        }
+
+        function createDiv02 (){
+                $("body").append ("<div data-guid=dv02></div>");
+                $("div[data-guid='dv02']").append ("<p>second div</p>")
+        }
+
+        function createDiv03 (){
+                $("body").append ("<div data-guid=div03></div>");
+                $("div[data-guid='dv03']").append ("<p>third dv</p>")
+        }
+
+       
 });
 
 
