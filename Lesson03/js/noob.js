44525720a345f6237da5f68f7e4238962edb4a3e
Nikos Glezakos <nick@doticca.com>
2018-11-05 22:32:24 +0200

    yes

--- Lesson03/js/noob.js
+++ Lesson03/js/noob.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
         createUI();
 
-        function createUI (){
+        function createUI(){
                 createDiv00();
                 createDiv01();
                 createDiv02();
@@ -13,27 +13,27 @@ $(document).ready(function(){
 
         // Create DIVs
 
-        function createDiv00 (){
+        function createDiv00(){
                 $("body").append ("<div data-guid='div00'></div>");
                 $("div[data-guid='div00'")
-                        .append ("<a href='#' data-target='div01' data-targettype='tab'> tab 1 </a>")
-                        .append ("<a href='#' data-target='div02' data-targettype='tab'> tab 2 </a>")
-                        .append ("<a href='#' data-target='div03' data-targettype='tab'> tab 3 </a>")
+                        .append("<a href='#' data-target='div01' data-targettype='tab'> tab 1 </a>")
+                        .append("<a href='#' data-target='div02' data-targettype='tab'> tab 2 </a>")
+                        .append("<a href='#' data-target='div03' data-targettype='tab'> tab 3 </a>")
         }
 
-        function createDiv01 (){
+        function createDiv01(){
                 $("body").append ("<div data-guid='div01' data-type='tab' class='hidden'></div>");
                 $("div[data-guid='div01']").append ("<p>first div</p>")
         }
 
-        function createDiv02 (){
+        function createDiv02(){
                 $("body").append ("<div data-guid='div02' data-type='tab' class='hidden'></div>");
-                $("div[data-guid='div02']").append ("<p>second div</p>")
+                $("div[data-guid='div02']").append("<p>second div</p>")
         }
 
-        function createDiv03 (){
+        function createDiv03(){
                 $("body").append ("<div data-guid='div03' data-type='tab' class='hidden'></div>");
-                $("div[data-guid='div03']").append ("<p>third div</p>")
+                $("div[data-guid='div03']").append("<p>third div</p>")
         }
 
 
@@ -42,17 +42,17 @@ $(document).ready(function(){
         handleTargets();
         
 
-        function handleTargets (){
-                $("a[data-targettype='tab']").off().on ("click", handleTab);
+        function handleTargets(){
+                $("a[data-targettype='tab']").off().on("click", handleTab);
                 
         }
 
-        function handleTab (e){
+        function handleTab(e){
                 tabcontent$=parent(element$).prop("data.guid");
                 hideAllTabs();
         }
 
-        function hideAllTabs (){
+        function hideAllTabs(){
                 $("a[data-targettype='tab'").addClass("hidden");
         }
 
